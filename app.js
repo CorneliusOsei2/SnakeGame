b0c16a89a37e8a7f338a76f4bf38d999f36df248
Cornelius <oseiowusuboatengcornelius6@gmail.com>
2021-08-15 01:10:44 -0400

    Snake movement started: up, down, left, right

--- app.js
+++ app.js
@@ -13,10 +13,14 @@ let tileSize = canvas.width / tileCount - 2;
 let headX = 10;
 let headY = 10;
 
-
+// Snake movement
+// Controlled by Keyboard
+let xVelocity = 0;
+let yVelocity = 0;
 
 const drawGame = () => {
     clearScreen();
+    changeSnakePosition();
     drawSnake();
     setTimeout(drawGame, 1000 / speed)
 }
@@ -26,4 +30,57 @@ const clearScreen = () => {
     ctx.fillRect(0, 0, canvas.width, canvas.height)
 }
 
-drawGame()
+
+const drawSnake = () => {
+    ctx.fillStyle = 'orange'
+    ctx.fillRect(headX * tileCount, headY * tileCount, tileCount, tileCount)
+}
+
+const changeSnakePosition = () => {
+    headX = headX + xVelocity;
+    headY = headY + yVelocity;
+}
+
+
+const keyMovement = (event) => {
+    // Go up
+    if (event.keyCode == 38) {
+        if (yVelocity == 1) {
+            return
+        }
+        yVelocity = -1;
+        xVelocity = 0;
+    }
+
+    // Go down
+    if (event.keyCode == 40) {
+        if (yVelocity == -1) {
+            return
+        }
+        yVelocity = 1
+        xVelocity = 0
+    }
+
+    // Go left
+    if (event.keyCode == 37) {
+        if (xVelocity == 1) {
+            return
+        }
+        yVelocity = 0
+        xVelocity = -1
+    }
+
+    // Go right
+    if (event.keyCode == 39) {
+        if (xVelocity == -1) {
+            return
+        }
+        yVelocity = 0
+        xVelocity = 1
+    }
+}
+
+document.body.addEventListener('keydown', keyMovement)
+
+
+drawGame();
